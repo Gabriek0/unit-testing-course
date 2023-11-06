720181ff56e99238f798000b959a253ec090909a
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-11-06 15:18:44 -0300

    feat: add AAA pattern with sumNumbers constant for sum

--- testing-frontend/src/math.spec.js
+++ testing-frontend/src/math.spec.js
@@ -2,8 +2,13 @@ import { expect, it } from "vitest";
 import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
+  // Arrange
   const numbers = [1, 2, 3, 4, 5];
+
+  // Act
   const sumFunction = add(numbers);
 
-  expect(sumFunction).toBe(15);
+  // Assert
+  const sumNumbers = numbers.reduce((acc, curr) => (acc += curr), 0);
+  expect(sumFunction).toBe(sumNumbers);
 });
